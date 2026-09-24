97fe18983bb0f34492e5f079855f8b1fff28df61
runeanielsen <runenielsen@runbox.com>
2026-09-24 23:14:17 +0200

    can now automatic update the versions

--- scripts/query-latest-image-update.sh
+++ scripts/query-latest-image-update.sh
@@ -14,7 +14,7 @@ query_img_version() {
     TOKEN=$3
     CURRENT_TAG=$4
 
-    # This is a bit ugly, but the best way I could figure out how to handle it with different version names.
+# This is a bit ugly, but the best way I could figure out how to handle it with different version names.
     version_regex="^[vV0-9]+.[0-9]+(.[0-9]+[a-zA-Z0-9-]{0,})?$"
     if [[ "$CURRENT_TAG" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
         version_regex="^v[0-9]+.[0-9]+.[0-9]+$"
@@ -52,30 +52,34 @@ find ./dax -type f -exec grep -Il -e "^image:$" {} + |
     while IFS= read -r file; do
         echo "Checking: $file"
 
-        repository=$(yq '(.. | select(kind == "map" and .repository != null) | .repository)' $file)
-        tag=$(yq '(.. | first(kind == "map" and .tag != null) | select(. != null) | .tag)' $file)
+        combined=$(yq -o=json '[(.. | select(kind == "map" and key == "image"))]' $file)
+        # echo $combined
 
         token=$(get_docker_token)
 
-        if [[ "$repository" =~ "/" ]]; then
-            organization="${repository%%/*}"
-            image_name="${repository#*/}"
-        else
-            organization="library"
-            image_name="$repository"
-        fi
-
-        newest_tag=$(query_img_version $organization $image_name $token $tag)
-
-        if [[ -n "$newest_tag" ]]; then
-            if [[ "$tag" != "$newest_tag" ]]; then
-                echo "Organization: $organization"
-                echo "Image name: $image_name"
-                echo "Updating $tag to $newest_tag"
-                echo ""
+        while IFS=$'\t' read -r repository tag; do
+            if [[ "$repository" =~ "/" ]]; then
+                organization="${repository%%/*}"
+                image_name="${repository#*/}"
+            else
+                organization="library"
+                image_name="$repository"
             fi
-        else
-            echo "Could not find tag for $image_name."
+
+            newest_tag=$(query_img_version $organization $image_name $token $tag)
+
+            if [[ -n "$newest_tag" ]]; then
+                echo "Checking for $repository in $file:"
+                if [[ "$tag" != "$newest_tag" ]]; then
+                    echo "Organization: $organization"
+                    echo "Image name: $image_name"
+                    echo "Updating $tag to $newest_tag"
+                    sed -i "/repository: ${repository%%/*}\/$image_name/{n;s/tag: $tag/tag: $newest_tag/;}" "$file"
+                fi
+            else
+                echo "Could not find tag for $image_name."
+            fi
+
             echo ""
-        fi
+        done < <(jq -r '.[] | [.repository, .tag] | @tsv' <<< "$combined")
     done
