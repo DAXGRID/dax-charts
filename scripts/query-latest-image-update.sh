97d60626cab3acc3cbed4f6c530a6113b2763d02
runeanielsen <runenielsen@runbox.com>
2026-09-25 10:48:55 +0200

    switch to using the newest tag

--- scripts/query-latest-image-update.sh
+++ scripts/query-latest-image-update.sh
@@ -98,7 +98,7 @@ find ./dax -type f -exec grep -Il -e "^image:$" {} + |
 
                     if [[ $chart_name == *"$image_name"* ]]; then
                         # Updates the appVersion in the chart file.
-                        sed -i "/appVersion:.*/c\appVersion: \"$new_chart_version\"" "$chart_path"
+                        sed -i "/appVersion:.*/c\appVersion: \"$newest_tag\"" "$chart_path"
                     fi
 
                     # Updates the version in the chart file.
